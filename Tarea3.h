6a77ade3ceae1232f016365b37a4181e9a9d4c6d
rimunoz <39927161+rimunoz@users.noreply.github.com>
2018-11-30 19:21:18 -0300

    Add files via upload

---
+++ Tarea3.h
@@ -0,0 +1,12 @@
+void printContainer(Container C);
+void printParticula(Particle P, int size);
+void imprimirContainer(Container C, Particle P);
+char getElement(int i, int j, Particle P,  Container C);
+struct particle  generarParticulas(Container C);
+void leerArgumentosEntrada(struct container *C , int argc , char *argv[]);
+int moveDuplicates(Container C, Particle P);
+void checkDuplicates(Container C, Particle P);
+struct particle obtenerDireccion(Container C, Particle P);
+void moverParticulas(Container C, Particle P, Particle P_next);
+void comprobarColisiones(Container C, Particle P);
+void desbloquearParticulas(Container C, Particle P);
